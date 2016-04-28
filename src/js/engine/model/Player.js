67982613746ead3a229c41f1c943f0deb0c61917
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-04-28 18:36:25 +0200

    Removing antialias, bigger sprite

--- src/js/engine/model/Player.js
+++ src/js/engine/model/Player.js
@@ -13,6 +13,7 @@ class Player {
                     this.posX,
                     this.posY,
                     this.BG);
+        sprite.scale.setTo(2, 2);
         sprite.anchor.setTo(0.5, 0.5);
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,8 @@ var PreloaderScene = require('./preloader-scene.js');
 var BootScene = require('./boot-scene.js');
 
 window.onload = function () {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game',
+            null, false, false);
 
     game.state.add('boot', BootScene);
     game.state.add('preloader', PreloaderScene);

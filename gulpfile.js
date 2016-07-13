383d780c77be75f0931816172344615c8861c8af
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-07-13 16:38:50 +0200

    Improving gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -9,23 +9,31 @@ var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
 var watchify = require('watchify');
 var browserSync = require('browser-sync').create();
+var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
 
-// var paths = {
-//     pages: ['src/*.html']
-// };
+var paths = {
+    pages: ['src/*.html'],
+    styles: ['src/styles.css'],
+    vendor: ['src/vendor/**'],
+    assets: ['src/images/**'],
+};
 
 const DEV_BUILD_PATH = '.tmp';
 const PROD_BUILD_PATH = 'dist';
+const VENDOR_PATH = '/vendor';
+const ASSETS_PATH = '/images';
 const ROOT_FILE = 'src/app/main.ts';
 
-var nonWatchedBrowserify = browserify({
+const browserifyOptions = {
     basedir: '.',
     debug: true,
     entries: [ROOT_FILE],
     cache: {},
     packageCache: {}
-}).plugin(tsify);
+};
 
+var nonWatchedBrowserify = browserify(browserifyOptions).plugin(tsify);
 var watchedBrowserify = watchify(nonWatchedBrowserify);
 
 function bundle() {
@@ -40,10 +48,25 @@ function bundle() {
         .pipe(browserSync.stream({once: true}));
 }
 
-// gulp.task('copy-html', function () {
-//     return gulp.src(paths.pages)
-//         .pipe(gulp.dest(DEV_BUILD_PATH));
-// });
+gulp.task('copy-html-dist', function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest(PROD_BUILD_PATH));
+});
+
+gulp.task('copy-css-dist', function () {
+    return gulp.src(paths.styles)
+        .pipe(gulp.dest(PROD_BUILD_PATH));
+});
+
+gulp.task('copy-vendor-dist', function () {
+    return gulp.src(paths.vendor)
+        .pipe(gulp.dest(PROD_BUILD_PATH + VENDOR_PATH));
+});
+
+gulp.task('copy-assets-dist', function () {
+    return gulp.src(paths.assets)
+        .pipe(gulp.dest(PROD_BUILD_PATH + ASSETS_PATH));
+});
 
 gulp.task('default', ['run'], bundle);
 
@@ -59,10 +82,26 @@ gulp.task('run', function () {
     gulp.watch('src/**/*.{html,css}').on('change', browserSync.reload);
 });
 
-// gulp.task('build', function() {
+gulp.task('dist', function (callback) {
+    runSequence('clean-dist',
+              ['build-dist', 'copy-html-dist', 'copy-css-dist', 'copy-vendor-dist', 'copy-assets-dist'],
+              callback);
+});
 
-// });
+gulp.task('build-dist', function () {
+    return browserify(browserifyOptions)
+    .plugin(tsify)
+    .transform('babelify')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest(PROD_BUILD_PATH));
+});
 
+gulp.task('clean-dist', function () {
+    return gulp.src(PROD_BUILD_PATH, {read: false})
+        .pipe(clean());
+});
 
 
 // 

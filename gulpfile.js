721c421ac2f7de274e9f6f6361ac4bccbed4c618
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-07-18 19:28:05 +0200

    GH pages

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var watchify = require('watchify');
 var browserSync = require('browser-sync').create();
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
+var ghpages = require('gulp-gh-pages');
 
 var paths = {
     pages: ['src/*.html'],
@@ -104,103 +105,7 @@ gulp.task('clean-dist', function () {
         .pipe(clean());
 });
 
-
-// 
-
-// var browserify = require('browserify');
-// 
-// var source = require('vinyl-source-stream');
-
-// 
-// var del = require('del');
-// var merge = require('merge-stream');
-
-
-// var ghpages = require('gulp-gh-pages');
-
-
-
-// //
-// // browserify and js
-// //
-
-// var bundler = browserify([
-//   './src/js/main.js'
-// ]);
-
-// var bundle = function ()  {
-//   return bundler
-//     .bundle()
-//     .on('error', gutil.log)
-//     .pipe(source('bundle.js'))
-//     .pipe(gulp.dest('.tmp/js/'))
-//     .pipe(browserSync.stream({once: true}));
-// };
-
-
-// gulp.task('browserify', bundle);
-
-// // 3rd party libs that don't play nice with browserify
-// gulp.task('libs', function () {
-//   var dir = './node_modules/phaser/dist/';
-//   return gulp.src(['phaser.min.js', 'phaser.map'], { cwd: dir, base: dir})
-//     .pipe(gulp.dest('./.tmp/js/lib/'));
-// });
-
-// gulp.task('js', ['browserify', 'libs']);
-
-// //
-// // build and deploy
-// //
-
-// gulp.task('build', ['js']);
-
-// gulp.task('dist', ['build'], function () {
-//   var rawFiles = gulp.src([
-//     'index.html', 'raw.html',
-//     'styles.css',
-//     'images/**/*', 'fonts/**/*', 'audio/**/*'
-//   ], { cwd: './src', base: './src' })
-//     .pipe(gulp.dest('./dist/'));
-
-//   var builtFiles = gulp.src(['js/**/*'], { cwd: '.tmp', base: '.tmp' })
-//     .pipe(gulp.dest('./dist/'));
-
-//   return merge(rawFiles, builtFiles);
-// });
-
-// gulp.task('clean', function () {
-//   return del(['.tmp', 'dist', '.publish']);
-// });
-
-
-// gulp.task('deploy:ghpages', ['dist'], function () {
-//   return gulp.src('dist/**/*')
-//     .pipe(ghpages());
-// });
-
-
-// gulp.task('deploy', ['deploy:ghpages'])
-
-// //
-// // dev tasks
-// //
-
-// gulp.task('watch', function () {
-//   bundler = watchify(bundler, watchify.args);
-//   bundler.on('update', bundle);
-// });
-
-// gulp.task('run', ['watch', 'build'], function () {
-//   browserSync.init({
-//     server: ['src', '.tmp']
-//   });
-
-//   gulp.watch('src/**/*.{html,css}').on('change', browserSync.reload);
-// });
-
-// //
-// // default task
-// //
-
-// gulp.task('default', ['dist']);
+gulp.task('deploy', ['dist'], function () {
+    return gulp.src('dist/**/*')
+        .pipe(ghpages());
+});

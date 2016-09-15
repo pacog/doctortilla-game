94e9e62d9d4391ca4a1c9d43c78d3fa1a9908549
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-09-15 16:36:34 +0200

    Explodig the balloon logic and final speech

--- src/app/game/backyardScene/Balloon.ts
+++ src/app/game/backyardScene/Balloon.ts
@@ -2,6 +2,10 @@ import { Thing } from '../../engine/models/Thing';
 import { Directions } from '../../engine/utils/Directions';
 import { style } from '../../engine/ui/Style';
 import { DoctortillaPlayer } from '../DoctortillaPlayer';
+import { selectedThing } from '../../engine/state/SelectedObjects';
+import { scenes } from '../../engine/state/Scenes.singleton';
+import { Bili } from './Bili';
+import { uiBlocker } from '../../engine/ui/UIBlocker.singleton';
 
 let spriteOptions = new Map();
 
@@ -37,6 +41,19 @@ export class Balloon extends Thing {
         this.playAnimationSometime();
     }
 
+    protected useAction(player: DoctortillaPlayer): void {
+        if (selectedThing.thing.id === 'scissors') {
+            let bili = <Bili> scenes.getSceneById('BACKYARD').getThingById('bili');
+            if(bili.getAttr('DRUNK')) {
+                this.explodeBalloon(player, bili);
+            } else {
+                player.say('HE_IS_BEING_GOOD_FOR_NOW_NO_NEED_TO_EXPLODE_IT');
+            }
+        } else {
+            super.useAction(player);
+        }
+    }
+
     protected lookAction(player: DoctortillaPlayer): void {
         player.say('THAT_IS_A_HUGE_BALLOON_QUITE_SCARY_IF_IT_EXPLODES');
     }
@@ -57,5 +74,58 @@ export class Balloon extends Thing {
     private getTimeForNextAnimation(): number {
         return MIN_TIME_FOR_ANIMATION + Math.random()*(MAX_TIME_FOR_ANIMATION - MIN_TIME_FOR_ANIMATION);
     }
+
+    private explodeBalloon(player: DoctortillaPlayer, bili: Bili): void {
+        uiBlocker.block();
+        player.goToThing(this)
+            // .then(() => {
+            //     return player.playAnimationOnce('explode_balloon');
+            // })
+            // .then(() => {
+            //     return this.playAnimationOnce('explode');
+            // })
+            .then(() => {
+                this.changeAttr('EXPLODED', true);
+                return bili.say('I_AM_AWAKE');
+            })
+            .then(() => {
+                return bili.say('I_AM_NOT_DRUNK_ANYMORE');
+            })
+            .then(() => {
+                return bili.say('WE_CAN_PLAY_THE_CONCERT_NOW');
+            })
+            .then(() => {
+                return player.moveTo({ x: 181, y: 212});
+            })
+            .then(() => {
+                player.lookAt(Directions.DOWN);
+                return player.say('SO_THIS_IS_THE_END_FOLKS');
+            })
+            .then(() => {
+                return player.say('WE_LAUGHED');
+            })
+            .then(() => {
+                return player.say('WE_CRIED');
+            })
+            .then(() => {
+                return player.say('AND_WE_LEARNED');
+            })
+            .then(() => {
+                return player.wait(1000);
+            })
+            .then(() => {
+                return player.say('THANKS_FOR_HELPING_ME_IN_THIS_ADVENTURE');
+            })
+            .then(() => {
+                return player.say('SEE_YOU_SOON');
+            })
+            .then(() => {
+                uiBlocker.unblock();
+                //TODO Go to credits
+            });
+    }
+
+    //TODO: create animation
+    //TODO: react to state change
 
 }

b5c14304456149a347c7a545a2740f96cb7ddb14
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-09-21 18:45:22 +0200

    Disabling mobile devices

--- src/app/main.ts
+++ src/app/main.ts
@@ -8,15 +8,26 @@ import { playScene } from './PlayScene';
 import { phaserGame } from './engine/state/PhaserGame.singleton';
 
 window.onload = function() {
-    let game = new phaser.Game(layout.WIDTH, layout.HEIGHT, phaser.AUTO, 'game');
+    if(!isMobile()) {
+        let game = new phaser.Game(layout.WIDTH, layout.HEIGHT, phaser.AUTO, 'game');
 
-    phaserGame.value = game;
+        phaserGame.value = game;
 
-    game.state.add('boot', BootScene);
-    game.state.add('preloader', PreloaderScene);
-    game.state.add('startMenu', StartMenuScene);
-    game.state.add('play', playScene);
-    game.state.add('credits', CreditsScene);
+        game.state.add('boot', BootScene);
+        game.state.add('preloader', PreloaderScene);
+        game.state.add('startMenu', StartMenuScene);
+        game.state.add('play', playScene);
+        game.state.add('credits', CreditsScene);
 
-    game.state.start('boot');
+        game.state.start('boot');
+    }
 };
+
+function isMobile() {
+    const MIN_WIDTH = 400;
+    const MIN_HEIGHT = 400;
+    let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    return (w < MIN_HEIGHT) || (h < MIN_HEIGHT);
+}
